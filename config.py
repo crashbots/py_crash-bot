351c9544653d720003368468efc89e2bb81f6199
TerminalPR0 <69242996+TerminalPR0@users.noreply.github.com>
2022-07-03 13:41:06 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,5 @@
 settings = {
-  "prefix": '!',
-  "token": 'ODM3MzIyMDcwNDQ1NjU0MDQ3.YIq28g.Mf1snnxGLXlm_3U7lJc7GSO-BlQ',
+  "prefix": '!'
   "status": 'discord.Status.do_not_disturb',
   "statustext": 'Крашит пентагон...',
   "delmessagesamount": 100000
